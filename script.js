42071fa9908d0269cfa388f59efeca685237fb1b
nirmallyakoner <79015427+nirmallyakoner@users.noreply.github.com>
2023-06-19 19:55:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const findmyLocation = () => {
         console.log(position);
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-        console.log(latitude + "" + longitude);
+        console.log(latitude + "   " + longitude);
 
         
         // const geoAPI = `http://api.ipstack.com/2409:40e1:2b:6f34:64b0:689c:3a97:3ec6?access_key=7c26bbd0fd1d4bfa8e7400b482b6a531&format=1`
