6718675d5b47a5e630fa6c0289b949b0bb61540d
nirmallyakoner <79015427+nirmallyakoner@users.noreply.github.com>
2023-06-19 19:25:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const findmyLocation= ()=>{
-    const status=document.getElementById("status");
+const findmyLocation = () => {
+    const status = document.getElementById("status");
 
-const success=(position)=>{
-    console.log(position);
-    const latitude=position.coords.latitude;
-    const longitude=position.coords.longitude;
-    console.log(latitude+""+longitude);
+    const success = (position) => {
+        console.log(position);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        // console.log(latitude + "" + longitude);
+
+        const geoAPI = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+        fetch(geoAPI)
+            .then(res => res.json())
+            .then(data => {
+                status.textContent = data.principalSubdivision
+            })
+    }
+
+    const error = () => {
+        status.innerHTML = "Error"
+    }
+
+
+
+    navigator.geolocation.getCurrentPosition(success, error);
 }
 
-const error=()=>{
-    status.innerHTML="Error"
-}
-
-
-
-    navigator.geolocation.getCurrentPosition(success, error); 
-}
-
-document.getElementById("location").addEventListener("click",findmyLocation);
+document.getElementById("location").addEventListener("click", findmyLocation);
