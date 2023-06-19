fdc25f74ca29a21582261f156708a3603f8d201e
nirmallyakoner <79015427+nirmallyakoner@users.noreply.github.com>
2023-06-19 19:39:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const findmyLocation = () => {
         const longitude = position.coords.longitude;
         // console.log(latitude + "" + longitude);
 
-        const geoAPI = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+        
+        const geoAPI = `http://api.ipstack.com/2409:40e1:2b:6f34:64b0:689c:3a97:3ec6?access_key=7c26bbd0fd1d4bfa8e7400b482b6a531&format=1`
 
         fetch(geoAPI)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                status.textContent = data.principalSubdivision;
+                status.textContent = data.zip;
             })
     }
 
